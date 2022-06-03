7e40a9e0153d6417f4a47c2b53fd043a14b98a31
Denis-alt-coder <74229007+Denis-alt-coder@users.noreply.github.com>
2022-06-03 19:55:44 +0300

    Added starting menu.

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,14 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
-
-from loader import dp
+from aiogram.types import InputFile
+from loader import dp, bot
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Привет, {message.from_user.full_name}!")
+    photo = InputFile('D:/Downloads/PYTHON/telebot/handlers/users/bot_photo.jpg')
+    await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+    await bot.send_photo(chat_id=message.chat.id, photo=photo)
+    await message.answer(f"Здравствуйте, {message.from_user.full_name}!"
+                         f"Добро пожаловать! Данный бот поможет вам выбрать тариф в складчину и расскажет о сервисе.")
+
