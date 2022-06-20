800df1ba0e198980bd50be987ea9c69d69e55328
Denis-alt-coder <74229007+Denis-alt-coder@users.noreply.github.com>
2022-06-20 17:28:03 +0300

    Added

--- bot.py
+++ bot.py
@@ -1,12 +1,4 @@
 from aiogram import executor
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-from data import config
-
-bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
 
 if __name__ == '__main__':
     executor.start_polling(dp)
