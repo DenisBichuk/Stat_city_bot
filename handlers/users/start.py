ca8759189c158b473de19062933bdf7dc23141d0
Denis_alt_coder <denisbicuk18@gmail.com>
2022-07-08 16:17:44 +0300

    Changed the way to photos and fixed weak warnings

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -8,7 +8,6 @@ from .states import FSMProf, FSMPrem, FSMComposite, FSMTop, FSMTotalDom
 
 @dp.message_handler(commands=['start'])
 async def bot_start(message: types.Message):
-    await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
     photo = config.BOT_PHOTO_ID
     await bot.send_photo(chat_id=message.chat.id, photo=photo)
     await message.answer(
